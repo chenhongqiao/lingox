22278e3f03d53277712bef1cf4e61ea15a8217da
Harry Chen <harrychen0314@gmail.com>
2022-06-15 22:27:26 -0700

    prototype

---
+++ configs/index.js
@@ -0,0 +1,12 @@
+import theme from './theme'
+
+export default {
+  // product display information
+  product: {
+    name: 'LingoX Website',
+    version: 'v1.0.0'
+  },
+
+  // theme configs
+  theme
+}
---
+++ configs/theme.js
@@ -0,0 +1,18 @@
+export default {
+  // global theme for the app
+  globalTheme: 'light', // light | dark
+
+  // light theme colors
+  light: {
+    textbase: '#222222',
+    background: '#ffffff',
+    surface: '#f5f5f5',
+    primary: '#e27262',
+    secondary: '#363636',
+    accent: '#32B0C0',
+    error: '#ef476f',
+    info: '#2196F3',
+    success: '#05c075',
+    warning: '#ffd166'
+  }
+}
