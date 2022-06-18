ecc2af17ae0f804013f7b2bd2da2b9cdca23a1d4
Harry Chen <harrychen0314@gmail.com>
2022-06-18 10:26:00 -0700

    update faq

--- pages/about/faq.vue
+++ pages/about/faq.vue
@@ -36,16 +36,19 @@ export default defineComponent({
     const FAQs: Ref<FAQ[]> = ref([
       {
         question: 'What is the cost of enrolling in a LingoX program?',
-        answer: 'None at all! LingoX strives to break down barriers in education by eliminating fees.'
+        answer: 'As part of our commitment to making education accessible to everyone, enrollment in any LingoX program is offered free of charge thanks to our wonderful volunteers and kind sponsors!'
       }, {
         question: 'When are the lessons?',
-        answer: 'You decide! Once a match is finalized, contact details will be provided for the tutor communicate with the student to select a time that works best for both parties.'
+        answer: 'We understand that many of our students may have very busy schedules. As such, LingoX offers personalized curricula with flexible scheduling, meaning that students can choose what time works best for them and at what pace they wish to learn. Once the student is matched with a LingoX Tutor, contact details will be provided for both parties to arrange a time that works best.'
       }, {
         question: 'Can I select my tutor?',
-        answer: 'Unfortunately, due to logistical reasons, LingoX staff will be responsible for matching students and tutors. However, you can rest assured that all LingoX tutors are knowledgeable, patient, and supportive.'
+        answer: 'Due to logistical reasons, LingoX staff will be responsible for matching students and tutors. All LingoX tutors undergo an extensive approval process, so parents and students can rest assured that not only are our tutors incredibly patient and supportive toward all types of learners, but they also maintain extensive knowledge of the curriculum.'
       }, {
         question: 'Is it possible to change my tutor after a few lessons?',
-        answer: 'If, after 1-2 sessions, you feel that the tutor cannot properly address your needs, you may email contact@lingox.org to request a change. However, placement is dependent on availability and an alternative selection is not guaranteed.'
+        answer: 'We strive to provide only the most qualified tutors to support student learning. If, after 1-2 sessions, you feel that your tutor cannot properly address your needs, you may email contact@lingox.org to request a change. However, placement is dependent on availability and an alternative selection cannot be guaranteed.'
+      }, {
+        question: 'Am I guaranteed a tutor?',
+        answer: 'To this day, LingoX has yet to leave a student unmatched. Should there be an unanticipated increase in students, we will try our best to meet everyone’s demand; however, please note that LingoX may prioritize certain populations depending on assessed need, such as children currently unable to attend school due to war or natural disasters.'
       }
     ])
 
