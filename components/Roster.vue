babb85f67e4b062941ed1b2a99e0f04d0757060d
Harry Chen <harrychen0314@gmail.com>
2022-06-17 19:22:45 -0700

    make roster a component

---
+++ components/Roster.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <div v-for="(person, i) in team" :key="i">
+      <v-divider v-if="i !== 0" />
+      <v-row class="py-3" align="center">
+        <v-col cols="3">
+          <v-img :src="person.image" max-height="400" class="elevation-6 rounded" />
+        </v-col>
+        <v-col>
+          <div class="text-h5">
+            {{ person.name }}
+          </div>
+          <div v-if="person.position" class="text-h6 secondary--text">
+            {{ person.position }}
+          </div>
+          <div class="text-body-1 mt-3">
+            {{ person.bio }}
+          </div>
+        </v-col>
+      </v-row>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+export interface Member {
+  name: string
+  position?: string
+  bio: string
+  image: string
+}
+export default defineComponent({
+  name: 'RosterComponent',
+  props: {
+    team: {
+      type: Array as () => Member[],
+      required: true
+    }
+  }
+})
+</script>
