0c6b185975f7c48ab40a0653730d202f557213a3
Harry Chen <harrychen0314@gmail.com>
2022-06-18 15:09:51 -0700

    remove default favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,9 +14,6 @@ export default {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
 
