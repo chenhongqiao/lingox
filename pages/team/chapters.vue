b2abb5a7cc1f6febcd340c964325e51aaa51f054
mlee-123 <108111430+mlee-123@users.noreply.github.com>
2022-09-02 08:34:53 -0700

    Update chapters.vue

--- pages/team/chapters.vue
+++ pages/team/chapters.vue
@@ -169,7 +169,7 @@ export default defineComponent({
         name: 'UK',
         members: [
           {
-            name: 'Alvin Chen',
+            name: 'Alvin Zhou',
             position: 'Director of Development',
             bio: 'bio.alvin',
             image: $img('team/alvin.png')
